e4a00cc63271316b5eb2bb9bb1cd6e3fc5ecfa15
amatureApe <amatureape@gmail.com>
2021-12-01 19:23:11 +0100

    built controller

---
+++ pickleFarms (solar)/interfaces/controller.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.6.0;
+
+interface IController {
+    function jars(address) external view returns (address);
+
+    function rewards() external view returns (address);
+
+    function devFund() external view returns (address);
+
+    function treasury() external view returns (address);
+
+    function balanceOf(address) external view returns (uint256);
+
+    function withdraw(address, uint256) external;
+
+    function withdrawReward(address, uint256) external;
+
+    function earn(address, uint256) external;
+
+    function strategies(address) external view returns (address);
+}
